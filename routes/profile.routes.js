d913ea14e7487c5aa91e19be5d988250c823bf80
“Ilke <ilkedidem@gmail.com>
2023-05-26 11:19:29 +0200

    login edit

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,23 +1,33 @@
 const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.post("/edit", async (req,res,next) => {
-
+router.post("/edit", async (req, res, next) => {
   const potentialUser = await User.findOne({ email: req.body.email });
-  const payload = req.body;
+  let payload = req.body;
+  if (payload.password) {
+    const salt = bcrypt.genSaltSync(9);
+    payload.password = bcrypt.hashSync(payload.password, salt);
+  } else {
+    const { password, ...rest } = payload;
+    payload = rest;
+  }
   try {
-    const updatedProfile = await User.findByIdAndUpdate(potentialUser._id, payload, {
-      new: true,
-    });
+    const updatedProfile = await User.findByIdAndUpdate(
+      potentialUser._id,
+      payload,
+      {
+        new: true,
+      }
+    );
     res.status(200).json(updatedProfile);
   } catch (error) {
     console.log(error);
   }
-})
-
+});
 
 module.exports = router;
